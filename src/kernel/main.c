5462c3abb874f61313aee58b09d17d11e942377b
tact1m4n3 <tudor.andrei.dicu@gmail.com>
2021-04-15 18:00:36 +0300

    First commit

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -22,28 +22,7 @@ typedef struct
 	void* glyphBuffer;
 } PSF1_FONT;
 
-void PutChar(FrameBuffer* frameBuffer, PSF1_FONT* psf1Font, unsigned int color, char c, unsigned int xOff, unsigned int yOff)
-{
-    unsigned int* pixPtr = (unsigned int*)frameBuffer->BaseAddress;
-    char* fontPtr = psf1Font->glyphBuffer + (c * psf1Font->psf1Header->charSize);
-
-    for (unsigned long y = yOff; y < yOff + 16; ++y)
-    {
-        for (unsigned long x = xOff; x < xOff + 8; ++x)
-        {
-            if ((*fontPtr & (0b10000000 >> (x - xOff))) > 0)
-            {
-                *(unsigned int*)(pixPtr + x + (y * frameBuffer->PixelsPerScanLine)) = color;
-            }
-        }
-
-        ++fontPtr;
-    }
-}
-
 void main(FrameBuffer* frameBuffer, PSF1_FONT* psf1Font)
 {
-    PutChar(frameBuffer, psf1Font, 0xffffffff, 'G', 20, 20);
-
     return;
 }
